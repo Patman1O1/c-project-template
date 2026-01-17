3479b8e38e7240431aaccf5051dbeea7081f3b7e
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-17 15:18:01 -0600

    Add src/main.c

--- src/main.c
+++ src/main.c
@@ -1,6 +1,14 @@
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif // ifdef __cplusplus
+
 int main(int argc, char** argv) {
 
     return 0;
 }
+
+#ifdef __cplusplus
+}
+#endif // ifdef __cplusplus
