c2e9aadebf7d65bb9765986e66f51bccb53491a3
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-17 14:59:19 -0600

    Add export.h

---
+++ include/__template__/export.h
@@ -0,0 +1,12 @@
+#ifndef EXPORT_H
+#define EXPORT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif // ifdef __cplusplus
+
+#ifdef __cplusplus
+}
+#endif // ifdef __cplusplus
+
+#endif // ifndef EXPORT_H
