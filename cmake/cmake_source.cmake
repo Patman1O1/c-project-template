b20542c96050d119041f56611db5063ef1926be5
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-18 17:04:20 -0600

    Edit cmake_source.cmake

--- cmake/cmake_source.cmake
+++ cmake/cmake_source.cmake
@@ -4,7 +4,7 @@
 # Define parameters
 set(PROJECT_NAME "" CACHE STRING "PROJECT_NAME") # Required
 set(SOURCE_NAME "" CACHE STRING "SOURCE_NAME") # Required
-set(CREATE_HEADER OFF CACHE STRING "CREATE_HEADER")
+set(CREATE_HEADER "" CACHE STRING "CREATE_HEADER")
 
 # Variables
 string(TOUPPER "${SOURCE_NAME}" SOURCE_NAME_UPPER)
@@ -25,29 +25,39 @@ if(NOT SOURCE_NAME)
 endif()
 
 # Ensure only "ON" or "OFF" are passed in as an argument for the CREATE_HEADER parameter
-if(NOT CREATE_HEADER MATCHES "^(ON|OFF)$")
+if(CREATE_HEADER MATCHES "ON")
+    set(CREATE_HEADER ON)
+elseif(CREATE_HEADER MATCHES "OFF" OR CREATE_HEADER MATCHES "")
+    set(CREATE_HEADER OFF)
+else()
     message(FATAL_ERROR "\"CREATE_HEADER\" expected ON or OFF as an argument but received ${CREATE_HEADER}")
     return()
 endif()
 
 # Check if the source file already exists
 if(EXISTS "${CMAKE_SOURCE_DIR}/src/${SOURCE_NAME}.c")
-    message(NOTICE "${SOURCE_NAME} already exists")
+    message(NOTICE "${SOURCE_NAME}.c already exists")
     return()
 endif()
 
 # Create the source file
 file(TOUCH "${CMAKE_SOURCE_DIR}/src/${SOURCE_NAME}.c")
-
-# Create the associated header file if it doesn't already exist and CREATE_HEADER is ON
-if(NOT EXISTS "${CMAKE_SOURCE_DIR}/include/${PROJECT_NAME}/${SOURCE_NAME}.h" AND CREATE_HEADER MATCHES "ON")
-    # Create the associated header file
-    execute_process(COMMAND "${CMAKE_COMMAND} -D PROJECT_NAME=${PROJECT_NAME} -D HEADER_NAME=${SOURCE_NAME} -P ${CMAKE_SOURCE_DIR}/cmake/cmake_header.cmake")
-endif()
-
-# Include the header file in the source file
-file(WRITE "${CMAKE_SOURCE_DIR}/src/${SOURCE_NAME}.c" "#include <${PROJECT_NAME}/${SOURCE_NAME}.h>\n\n")
-
 file(APPEND "${CMAKE_SOURCE_DIR}/src/${SOURCE_NAME}.c"
         "#ifdef __cplusplus\nextern \"C\" {\n#endif // #ifdef __cplusplus\n\n#ifdef __cplusplus\n}\n#endif // #ifdef __cplusplus\n"
 )
+
+# Check if the associated header file already exists
+if(EXISTS "${CMAKE_SOURCE_DIR}/include/${PROJECT_NAME}/${SOURCE_NAME}.h")
+    # Let the user know that the associated header file already exists
+    message(NOTICE "${SOURCE_NAME}.h already exists")
+elseif(CREATE_HEADER)
+    # Create a copy of SOURCE_NAME and convert all it's characters to uppercase
+    string(TOUPPER "${SOURCE_NAME}" SOURCE_NAME_UPPER)
+
+    # Create the associated header file
+    file(TOUCH "${CMAKE_SOURCE_DIR}/include/${PROJECT_NAME}/${SOURCE_NAME}.h")
+    file(WRITE "${CMAKE_SOURCE_DIR}/include/${PROJECT_NAME}/${SOURCE_NAME}.h"
+            "#ifndef ${SOURCE_NAME_UPPER}_H\n#define ${SOURCE_NAME_UPPER}_H\n\n#ifdef __cplusplus\nextern \"C\" {\n#endif // #ifdef __cplusplus\n\n#ifdef __cplusplus\n}\n#endif // #ifdef __cplusplus\n\n#endif // #ifndef ${SOURCE_NAME_UPPER}_H\n"
+    )
+endif()
+
