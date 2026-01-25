0e05428c5499dc46dfd8b223af9d7b5ff7ffb933
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-24 20:58:20 -0600

    Add ProjectInit.cmake

---
+++ .github/cmake/static_library/ProjectInit.cmake
@@ -0,0 +1,10 @@
+# Set BUILD_SHARED_LIBS to false and set the library type to static
+option(BUILD_SHARED_LIBS "Build the primary target as a shared library" ON)
+
+# Declare the project primary target as a static library
+set(PROJECT_PRIMARY_TARGET "${PROJECT_NAME_SNAKE_CASE}")
+add_library("${PROJECT_PRIMARY_TARGET}")
+add_library("${PROJECT_NAMESPACE}"::"${PROJECT_NAME}" ALIAS "${PROJECT_PRIMARY_TARGET}")
+
+# Set the project's output name
+set(PROJECT_OUTPUT_NAME "${PROJECT_NAME_KEBAB_CASE}")
