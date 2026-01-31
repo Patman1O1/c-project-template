853158d4de367f6737f921bced2824305621b00e
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-30 18:40:01 -0600

    Edit interface_library_init.cmake

--- .github/cmake/interface_library/interface_library_init.cmake
+++ .github/cmake/interface_library/interface_library_init.cmake
@@ -1,4 +1,4 @@
 # Declare the project primary target as a interface library
-set(PROJECT_PRIMARY_TARGET "${PROJECT_NAME_SNAKE_CASE}" CACHE STRING "The primary target that this project will build")
+set(PROJECT_PRIMARY_TARGET "${PROJECT_NAME}" CACHE STRING "The primary target that this project will build")
 add_library("${PROJECT_PRIMARY_TARGET}" INTERFACE)
 add_library("${PROJECT_NAMESPACE}"::"${PROJECT_NAME}" ALIAS "${PROJECT_PRIMARY_TARGET}")
