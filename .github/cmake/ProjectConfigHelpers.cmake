1f1c7be01dc6159e2595d7427ae7e1caad114571
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-30 19:03:01 -0600

    Edit ProjectConfigHelpers.cmake

--- .github/cmake/ProjectConfigHelpers.cmake
+++ .github/cmake/ProjectConfigHelpers.cmake
@@ -11,23 +11,6 @@ set(PROJECT_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../..")
 #-----------------------------------------------------------------------------------------------------------------------
 # Functions
 #-----------------------------------------------------------------------------------------------------------------------
-function(configure_include_directory PROJECT_NAME)
-    # Rename the template subdirectory to the project name
-    file(RENAME "${PROJECT_ROOT_DIR}/include/template" "${PROJECT_ROOT_DIR}/include/${PROJECT_NAME}")
-
-    # Configure the project header file (i.e. ${PROJECT_NAME}.h)
-    configure_file("${PROJECT_ROOT_DIR}/include/${PROJECT_NAME}/template.h.in"
-                   "${PROJECT_ROOT_DIR}/include/${PROJECT_NAME}/${PROJECT_NAME}.h"
-                   @ONLY)
-    file(REMOVE "${PROJECT_ROOT_DIR}/include/${PROJECT_NAME}/template.h.in")
-
-    # Configure the export header file (i.e. export.h)
-    configure_file("${PROJECT_ROOT_DIR}/include/${PROJECT_NAME}/export.h.in"
-                   "${PROJECT_ROOT_DIR}/include/${PROJECT_NAME}/export.h"
-                   @ONLY)
-    file(REMOVE "${PROJECT_ROOT_DIR}/include/${PROJECT_NAME}/export.h.in")
-endfunction()
-
 function(configure_source_directory PROJECT_NAME PROJECT_NAMESPACE PROJECT_TYPE)
     if(PROJECT_TYPE MATCHES "^(static_library|shared_library)$")
         # Rename ${PROJECT_ROOT_DIR}/src/template to ${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}
