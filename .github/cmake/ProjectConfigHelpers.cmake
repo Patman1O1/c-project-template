6467987824167f08a706995de1890ec09e73645b
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-30 18:31:16 -0600

    Edit ProjectConfigHelpers.cmake

--- .github/cmake/ProjectConfigHelpers.cmake
+++ .github/cmake/ProjectConfigHelpers.cmake
@@ -44,52 +44,61 @@ function(configure_include_directory PROJECT_NAME)
 endfunction()
 
 function(configure_source_directory PROJECT_NAME PROJECT_NAMESPACE PROJECT_TYPE)
-    # Rename the "template" subdirectory to the project name
-    file(RENAME "${PROJECT_ROOT_DIR}/src/template" "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}")
-
-    # Configure the source directory CMakeLists.txt file
-    configure_file("${PROJECT_ROOT_DIR}/src/CMakeLists.txt.in"
-                   "${PROJECT_ROOT_DIR}/src/CMakeLists.txt"
-                    @ONLY)
-    file(REMOVE "${PROJECT_ROOT_DIR}/src/CMakeLists.txt.in")
-
     if(PROJECT_TYPE MATCHES "^(static_library|shared_library)$")
+        # Rename ${PROJECT_ROOT_DIR}/src/template to ${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}
+        file(RENAME "${PROJECT_ROOT_DIR}/src/template" "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}")
+
+        # Configure the source directory CMakeLists.txt
+        configure_file("${PROJECT_ROOT_DIR}/src/CMakeLists.txt.in"
+                       "${PROJECT_ROOT_DIR}/src/CMakeLists.txt"
+                       @ONLY)
+        file(REMOVE "${PROJECT_ROOT_DIR}/src/CMakeLists.txt.in")
+
+        # Configure the CMakeLists.txt file in ${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}
+        file(READ "${CMAKE_CURRENT_LIST_DIR}/${PROJECT_TYPE}/${PROJECT_TYPE}_definition.cmake" PROJECT_DEFINE)
+        configure_file("${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/CMakeLists.txt.in"
+                       "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/CMakeLists.txt"
+                       @ONLY)
+        file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/CMakeLists.txt.in")
+
         # Remove main.c.in (libraries don't have entry points)
         file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/main.c.in")
-    elseif(PROJECT_TYPE MATCHES "interface_library")
-        # Remove all source files (interface_libraries are header-only)
-        file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/main.c.in")
+
+        # Configure ${PROJECT_NAME}.c
+        configure_file("${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/template.c.in"
+                       "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/${PROJECT_NAME}.c"
+                       @ONLY)
         file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/template.c.in")
+    elseif(PROJECT_TYPE MATCHES "interface_library")
+        # Remove the source directory entirely (interface libraries are header only)
+        file(REMOVE_RECURSE "${PROJECT_ROOT_DIR}/src")
     else()
+        # Rename ${PROJECT_ROOT_DIR}/src/template to ${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}
+        file(RENAME "${PROJECT_ROOT_DIR}/src/template" "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}")
+
+        # Configure the source directory CMakeLists.txt
+        configure_file("${PROJECT_ROOT_DIR}/src/CMakeLists.txt.in"
+                       "${PROJECT_ROOT_DIR}/src/CMakeLists.txt"
+                       @ONLY)
+        file(REMOVE "${PROJECT_ROOT_DIR}/src/CMakeLists.txt.in")
+
+        # Configure the CMakeLists.txt file in ${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}
+        file(READ "${CMAKE_CURRENT_LIST_DIR}/${PROJECT_TYPE}/${PROJECT_TYPE}_definition.cmake" PROJECT_DEFINE)
+        configure_file("${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/CMakeLists.txt.in"
+                       "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/CMakeLists.txt"
+                       @ONLY)
+        file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/CMakeLists.txt.in")
+
         # Configure main.c
         configure_file("${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/main.c.in"
                        "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/main.c"
                        @ONLY)
         file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/main.c.in")
+
+        # Configure ${PROJECT_NAME}.c
+        configure_file("${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/template.c.in"
+                       "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/${PROJECT_NAME}.c"
+                       @ONLY)
+        file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/template.c.in")
     endif()
-
-    # Configure the source CMakeLists.txt.in file
-    file(READ "${CMAKE_CURRENT_LIST_DIR}/${PROJECT_TYPE}/${PROJECT_TYPE}_definition.cmake" PROJECT_DEFINE)
-    configure_file("${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/CMakeLists.txt.in"
-                   "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/CMakeLists.txt"
-                    @ONLY)
-    file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/CMakeLists.txt.in")
-
-    # Configure the .c source file
-    configure_file("${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/template.c.in"
-                   "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/${PROJECT_NAME}.c"
-                   @ONLY)
-    file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/template.c.in")
-
-    # Configure the .h header file
-    configure_file("${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/include/template.h.in"
-                   "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/include/${PROJECT_NAME}.h"
-                   @ONLY)
-    file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/include/template.h.in")
-
-    # Configure the export header file
-    configure_file("${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/include/export.h.in"
-                   "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/include/export.h"
-                   @ONLY)
-    file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_NAME}/include/export.h.in")
 endfunction()
