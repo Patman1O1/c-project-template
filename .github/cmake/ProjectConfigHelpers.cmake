6407f1b927663fb33b5aa9c875c196d966845c63
Patman1O1 <patrickthomas101x@gmail.com>
2026-01-30 18:59:37 -0600

    Edit ProjectConfigHelpers.cmake

--- .github/cmake/ProjectConfigHelpers.cmake
+++ .github/cmake/ProjectConfigHelpers.cmake
@@ -11,21 +11,6 @@ set(PROJECT_ROOT_DIR "${CMAKE_CURRENT_LIST_DIR}/../..")
 #-----------------------------------------------------------------------------------------------------------------------
 # Functions
 #-----------------------------------------------------------------------------------------------------------------------
-function(configure_root_directory PROJECT_NAME PROJECT_NAMESPACE PROJECT_VERSION PROJECT_DESCRIPTION PROJECT_TYPE)
-    # Configure the project root CMakeLists.txt file
-    file(READ "${CMAKE_CURRENT_LIST_DIR}/${PROJECT_TYPE}/${PROJECT_TYPE}_init.cmake" PROJECT_INIT)
-    configure_file("${PROJECT_ROOT_DIR}/CMakeLists.txt.in"
-            "${PROJECT_ROOT_DIR}/CMakeLists.txt"
-            @ONLY)
-    file(REMOVE "${PROJECT_ROOT_DIR}/CMakeLists.txt.in")
-
-    # Configure the conanfile.py file
-    configure_file("${PROJECT_ROOT_DIR}/conanfile.py.in"
-                   "${PROJECT_ROOT_DIR}/conanfile.py"
-                   @ONLY)
-    file(REMOVE "${PROJECT_ROOT_DIR}/conanfile.py.in")
-endfunction()
-
 function(configure_include_directory PROJECT_NAME)
     # Rename the template subdirectory to the project name
     file(RENAME "${PROJECT_ROOT_DIR}/include/template" "${PROJECT_ROOT_DIR}/include/${PROJECT_NAME}")
@@ -103,4 +88,6 @@ function(configure_source_directory PROJECT_NAME PROJECT_NAMESPACE PROJECT_TYPE)
     endif()
 endfunction()
 
-
+function(configure_test_directory PROJECT_NAME)
+    # Configure the test directory CMakeLists.txt file
+endfunction()
