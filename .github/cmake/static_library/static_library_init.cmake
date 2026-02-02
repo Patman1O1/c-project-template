16326e3c76acdd1dee326db2b87b2e2c41ea5c1e
Patman1O1 <patrickthomas101x@gmail.com>
2026-02-02 12:08:49 -0600

    Edit static_library_init.cmake

--- .github/cmake/static_library/static_library_init.cmake
+++ .github/cmake/static_library/static_library_init.cmake
@@ -1,5 +1,5 @@
 # Set BUILD_SHARED_LIBS to false and set the library type to static
-option(BUILD_SHARED_LIBS "Build the primary target as a shared library" ON)
+option(BUILD_SHARED_LIBS "Build the primary target as a shared library" OFF)
 
 # Declare the project primary target as a static library
 add_library("${PROJECT_NAME}")
