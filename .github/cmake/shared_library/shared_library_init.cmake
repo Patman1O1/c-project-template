8030ce75f55588bdd59c8893c39377a549f0a553
Patman1O1 <patrickthomas101x@gmail.com>
2026-02-03 00:42:07 -0600

    Edit shared_library_init.cmake

--- .github/cmake/shared_library/shared_library_init.cmake
+++ .github/cmake/shared_library/shared_library_init.cmake
@@ -1,4 +1,7 @@
+# Set the default of BUILD_SHARED_LIBS to ON
+option(BUILD_SHARED_LIBS "Build the library as a shared library" ON)
+
 # Declare the project primary target as a shared library
-set(PROJECT_PRIMARY_TARGET "${PROJECT_NAME}" CACHE STRING "The primary target that this project will build")
-add_library("${PROJECT_PRIMARY_TARGET}" SHARED)
-add_library("${PROJECT_NAMESPACE}"::"${PROJECT_NAME}" ALIAS "${PROJECT_PRIMARY_TARGET}")
+set(PROJECT_PRIMARY_TARGET "${PROJECT_NAME}" CACHE STRING "")
+add_library("${PROJECT_PRIMARY_TARGET}")
+add_library("${PROJECT_NAMESPACE}"::"${PROJECT_PRIMARY_TARGET}" ALIAS "${PROJECT_PRIMARY_TARGET}")
