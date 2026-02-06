172831fd3313df8dcba63f4793b3cb9d339483e3
Patman1O1 <patrickthomas101x@gmail.com>
2026-02-06 12:22:20 -0600

    Edit interface_library_definition.cmake

--- .github/cmake/interface_library_definition.cmake
+++ .github/cmake/interface_library_definition.cmake
@@ -4,9 +4,7 @@ add_library(${PROJECT_NAMESPACE}::${PROJECT_PRIMARY_TARGET} ALIAS ${PROJECT_PRIM
 
 # Include directories
 target_include_directories(${PROJECT_PRIMARY_TARGET}
-        PRIVATE
-            "${CMAKE_SOURCE_DIR}/include/${PROJECT_PRIMARY_TARGET}"
-        PUBLIC
+        INTERFACE
             "$<BUILD_INTERFACE:${CMAKE_SOURCE_DIR}/include>"
             "$<INSTALL_INTERFACE:${CMAKE_INSTALL_INCLUDEDIR}>"
 )
