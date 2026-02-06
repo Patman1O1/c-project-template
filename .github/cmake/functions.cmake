3a7322aaea8f17da0d3d9686e8f8fb5274fbbeeb
Patman1O1 <patrickthomas101x@gmail.com>
2026-02-06 09:15:08 -0600

    Edit to_snake_case() in functions.cmake

--- .github/cmake/functions.cmake
+++ .github/cmake/functions.cmake
@@ -66,6 +66,12 @@ endfunction()
 #
 #-----------------------------------------------------------------------------------------------------------------------
 function(to_snake_case INPUT_STRING OUTPUT_STRING)
+    # Ignore empty strings
+    if("${INPUT_STRING}" MATCHES "")
+        set(RESULT_STRING "" PARENT_SCOPE)
+        return()
+    endif()
+
     # Replace common delimiters with underscores
     string(REGEX REPLACE "[-. ]" "_" temp "${INPUT_STRING}")
 
