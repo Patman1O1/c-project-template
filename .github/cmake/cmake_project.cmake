17b13050f25a12f3d554035e7788751fe2f1d580
Patman1O1 <patrickthomas101x@gmail.com>
2026-02-06 12:13:08 -0600

    Edit cmake_project.cmake

--- .github/cmake/cmake_project.cmake
+++ .github/cmake/cmake_project.cmake
@@ -131,8 +131,9 @@ if(PROJECT_TYPE MATCHES "^(static_library|shared_library)$")
                    @ONLY)
     file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_PRIMARY_TARGET}/template.c.in")
 elseif(PROJECT_TYPE MATCHES "interface_library")
-    # Remove the ${PROJECT_PRIMARY_TARGET} directory entirely (interface libraries are header-only)
-    file(REMOVE_RECURSE "${PROJECT_ROOT_DIR}/src/${PROJECT_PRIMARY_TARGET}")
+    # Remove all source files
+    file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_PRIMARY_TARGET}/${PROJECT_PRIMARY_TARGET}.c")
+    file(REMOVE "${PROJECT_ROOT_DIR}/src/${PROJECT_PRIMARY_TARGET}/main.c")
 else()
     # Configure main.c
     configure_file("${PROJECT_ROOT_DIR}/src/${PROJECT_PRIMARY_TARGET}/main.c.in"
