ded9e0961f99432fd28dcbe9b79c807bb411c1e9
Patman1O1 <patrickthomas101x@gmail.com>
2026-02-05 19:12:32 -0600

    Remove executable_init.cmake

--- .github/cmake/executable/executable_init.cmake
+++
@@ -1,3 +0,0 @@
-# Declare the project primary target as an executable
-set(PROJECT_PRIMARY_TARGET "${PROJECT_NAME}" CACHE STRING "The project's primary target")
-add_executable("${PROJECT_PRIMARY_TARGET}")
