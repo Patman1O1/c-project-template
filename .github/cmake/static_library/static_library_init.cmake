1829e682950e035010687f856d86b5b96ff7b1a9
Patman1O1 <patrickthomas101x@gmail.com>
2026-02-05 19:14:49 -0600

    Remove static_library_init.cmake

--- .github/cmake/static_library/static_library_init.cmake
+++
@@ -1,7 +0,0 @@
-# Set the default value BUILD_SHARED_LIBS to OFF
-option(BUILD_SHARED_LIBS "Build the library as a shared library" OFF)
-
-# Declare the project primary target as a static library
-set(PROJECT_PRIMARY_TARGET "${PROJECT_NAME}" CACHE STRING "The project's primary target")
-add_library("${PROJECT_PRIMARY_TARGET}")
-add_library("${PROJECT_NAMESPACE}"::"${PROJECT_PRIMARY_TARGET}" ALIAS "${PROJECT_PRIMARY_TARGET}")
