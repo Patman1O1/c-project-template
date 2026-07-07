5b463b8755e234d27be3df84c3c527ad842a04d5
Patrick Thomas <patrickthomas101x@gmail.com>
2026-07-07 16:07:09 -0500

    Implement pascal and screaming case tests

--- .github/python/tests/test_format.py
+++ .github/python/tests/test_format.py
@@ -11,3 +11,12 @@ def test__to_screaming_case__camel_case_string():
     assert to_screaming_case("helloWorld") == "HELLO_WORLD"
     assert to_screaming_case("abcEasyAs123Abc") == "ABC_EASY_AS_123_ABC"
 
+def test__to_screaming_case__pascal_case_string():
+    assert to_screaming_case("PascalCase") == "PASCAL_CASE"
+    assert to_screaming_case("HelloWorld") == "HELLO_WORLD"
+    assert to_screaming_case("AbcEasyAs123Abc") == "ABC_EASY_AS_123_ABC"
+
+def test__to_screaming_case__screaming_case_string():
+    assert to_screaming_case("SCREAMING_CASE") == "SCREAMING_CASE"
+    assert to_screaming_case("HELLO_WORLD") == "HELLO_WORLD"
+    assert to_screaming_case("ABC_EASY_AS_123_ABC") == "ABC_EASY_AS_123_ABC"
